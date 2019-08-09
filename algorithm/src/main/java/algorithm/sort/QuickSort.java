d5653780bcead91172e54519fd57caba152afe7a
SantaOracle <524372977@qq.com>
2019-08-09 10:13:54 +0800

    init quick sort

---
+++ algorithm/src/main/java/algorithm/sort/QuickSort.java
@@ -0,0 +1,13 @@
+package algorithm.sort;
+
+/**
+ * 快排
+ * Create by peiheng.jiang on 2019/8/9
+ */
+public class QuickSort implements Sort{
+
+    @Override
+    public int[] sort(int[] array) {
+        return new int[0];
+    }
+}
---
+++ algorithm/src/main/java/algorithm/sort/Sort.java
@@ -0,0 +1,10 @@
+package algorithm.sort;
+
+/**
+ * 排序接口
+ * Create by peiheng.jiang on 2019/8/9
+ */
+public interface Sort {
+
+    int[] sort(int[] array);
+}
